2a4a14dac97351b8fe5fb9d47b3c83ce101f38d0
scoobycoder <scoobycoder@gmail.com>
2014-09-28 15:06:36 -0400

    refactoring to remove conditionals

--- src/main/java/StockHandler.java
+++ src/main/java/StockHandler.java
@@ -6,10 +6,9 @@ public class StockHandler {
 	private static final String AGED_BRIE = "Aged Brie";
 	private static final String SULFURAS_RAGNAROS = "Sulfuras, Hand of Ragnaros";
 	private static final String TAFKAL80ETC = "Backstage passes to a TAFKAL80ETC concert";
-	
+
 	private List<Item> itemList = new ArrayList<Item>();
-	
-	
+
 	public List<Item> inventoryReport() {
 		return itemList;
 	}
@@ -17,78 +16,58 @@ public class StockHandler {
 	public void addItem(Item item) {
 		itemList.add(item);
 	}
-	
-    public void updateQuality()
-    {
-        for (int itemNumber = 0; itemNumber < itemList.size(); itemNumber++)
-        {
-            if (notAgedBrie(itemNumber) && notTafkal80Etc(itemNumber)) 
-            {
-                if (availableStock(itemNumber))
-                {
-                    if (notSulfurasRagnaros(itemNumber))
-                    {
-                        removeItem(itemNumber);
-                    }
-                }
-            }
-            else
-            {
-                if (needToRestock(itemNumber))
-                {
-                    buyItem(itemNumber);
 
-                    if (isTafkal80Etc(itemNumber))
-                    {
-                        if (itemAboutToSpoil(itemNumber, 11))
-                        {
-                            restockNeededItems(itemNumber);
-                        }
+	public void updateQuality() {
+		for (int itemNumber = 0; itemNumber < itemList.size(); itemNumber++) {
+			if (notAgedBrie(itemNumber) && notTafkal80Etc(itemNumber)) {
+				if (availableStock(itemNumber)) {
+					if (notSulfurasRagnaros(itemNumber)) {
+						removeItem(itemNumber);
+					}
+				}
+			} else {
+				if (needToRestock(itemNumber)) {
+					buyItem(itemNumber);
 
-                        if (itemAboutToSpoil(itemNumber, 6))
-                        {
-                            restockNeededItems(itemNumber);
-                        }
-                    }
-                }
-            }
+					if (isTafkal80Etc(itemNumber)) {
+						restockItemsAboutToSpoil(itemNumber);
+					}
+				}
+			}
 
-            if (notSulfurasRagnaros(itemNumber))
-            {
-                itemAgedADay(itemNumber);
-            }
+			if (notSulfurasRagnaros(itemNumber)) {
+				itemAgedADay(itemNumber);
+			}
 
-            if (itemAboutToSpoil(itemNumber, 0))
-            {
-                if (notAgedBrie(itemNumber))
-                {
-                    if (notTafkal80Etc(itemNumber))
-                    {
-                        if (availableStock(itemNumber))
-                        {
-                            if (notSulfurasRagnaros(itemNumber))
-                            {
-                                removeItem(itemNumber);
-                            }
-                        }
-                    }
-                    else
-                    {
-                        removeAllQuantity(itemNumber);
-                    }
-                }
-                else
-                {
-                    restockNeededItems(itemNumber);
-                }
-            }
-        }
-    }
+			if (itemAboutToSpoil(itemNumber, 0)) {
+				if (notAgedBrie(itemNumber) && notTafkal80Etc(itemNumber)) {
+					if (availableStock(itemNumber)) {
+						if (notSulfurasRagnaros(itemNumber)) {
+							removeItem(itemNumber);
+						}
+					} else {
+						removeAllQuantity(itemNumber);
+					}
+				} else {
+					restockNeededItems(itemNumber);
+				}
+			}
+		}
+	}
+
+	private void restockItemsAboutToSpoil(int itemNumber) {
+		int daysTillSpoiled = 11;
+
+		while (itemAboutToSpoil(itemNumber, daysTillSpoiled)) {
+			restockNeededItems(itemNumber);
+
+			daysTillSpoiled -= 1;
+		}
+	}
 
 	private void restockNeededItems(int itemNumber) {
-		if (needToRestock(itemNumber))
-		{
-		    buyItem(itemNumber);
+		if (needToRestock(itemNumber)) {
+			buyItem(itemNumber);
 		}
 	}
 
@@ -97,11 +76,14 @@ public class StockHandler {
 	}
 
 	private void removeAllQuantity(int itemNumber) {
-		itemList.get(itemNumber).setQuality(itemList.get(itemNumber).getQuality() - itemList.get(itemNumber).getQuality());
+		itemList.get(itemNumber).setQuality(
+				itemList.get(itemNumber).getQuality()
+						- itemList.get(itemNumber).getQuality());
 	}
 
 	private void removeItem(int itemNumber) {
-		itemList.get(itemNumber).setQuality(itemList.get(itemNumber).getQuality() - 1);
+		itemList.get(itemNumber).setQuality(
+				itemList.get(itemNumber).getQuality() - 1);
 	}
 
 	private boolean notTafkal80Etc(int itemNumber) {
@@ -117,11 +99,13 @@ public class StockHandler {
 	}
 
 	private void buyItem(int itemNumber) {
-		itemList.get(itemNumber).setQuality(itemList.get(itemNumber).getQuality() + 1);
+		itemList.get(itemNumber).setQuality(
+				itemList.get(itemNumber).getQuality() + 1);
 	}
 
 	private void itemAgedADay(int itemNumber) {
-		itemList.get(itemNumber).setSellIn(itemList.get(itemNumber).getSellIn() - 1);
+		itemList.get(itemNumber).setSellIn(
+				itemList.get(itemNumber).getSellIn() - 1);
 	}
 
 	private boolean availableStock(int itemNumber) {
@@ -135,5 +119,5 @@ public class StockHandler {
 	private boolean needToRestock(int itemNumber) {
 		return itemList.get(itemNumber).getQuality() < 50;
 	}
-	
+
 }
